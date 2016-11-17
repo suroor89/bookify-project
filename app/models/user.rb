8e1ad0aa07a8f4b35c9f5c183e0b814d4c1db200
quoc <squoc.nguyen@gmail.com>
2016-11-17 17:32:13 -0600

    add User MVC

---
+++ app/models/user.rb
@@ -0,0 +1,46 @@
+class User < ApplicationRecord
+
+  attr_accessor :remember_token
+
+  before_save :downcase_email
+
+  has_secure_password
+
+  validates_presence_of :firstname, :lastname, :email
+  validates :firstname, length: { maximum: 50 }
+  validates :lastname, length: { maximum: 50 }
+  validates :email, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  private
+  def downcase_email
+    self.email = email.downcase
+  end
+end
